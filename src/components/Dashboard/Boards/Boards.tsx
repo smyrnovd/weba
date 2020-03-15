10c140a4533a5d5bddc202f1ce6b98041aafe314
Dmitry Smyrnov <smyrnovd@gmail.com>
2020-03-15 14:02:35 +0200

    fetch

---
+++ src/components/Dashboard/Boards/Boards.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Boards = (props: any) => {
+  return <h2>{props.board_name}</h2>;
+};
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { removeToken } from "../../utils/removeToken";
 import { getFromLocalStorage } from "../../utils";
+import { Boards } from "./Boards";
 
 export const Dashboard = (props: any) => {
   const { REACT_APP_API_KEY } = process.env;
   const token = getFromLocalStorage();
   const url = `https://api.trello.com/1/members/me/boards?filter=all&fields=all&lists=none&memberships=none&organization=false&organization_fields=name%2CdisplayName&key=${REACT_APP_API_KEY}&token=${token}`;
-  console.log(fetch(url));
+
+  const DeskLoader = (url: string) => {
+    fetch(url).then(response => {
+      response.json().then(json => {
+        console.log(json);
+      });
+    });
+  };
+  const res = DeskLoader(url);
+  console.log(res);
+
   return (
     <div>
-      DASHBOARD
-      <p>{token}</p>
+      <h1>DASHBOARD</h1>
+
+      <Boards board_name="hohohoh" />
+
       <a href={process.env.REACT_APP_TRELLO_URL} onClick={() => removeToken()}>
         You can remove token with this link
       </a>
